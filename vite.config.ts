b0336867bc8165168c0ceb2566bf4f15348f3f59
Zaahid <zaahidismail6@gmail.com>
2025-08-16 12:03:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./client/src"),
       "@shared": path.resolve(__dirname, "./shared"),
-      "@assets": path.resolve(__dirname, "./client/src/assets")
+      "@assets": path.resolve(__dirname, "./attached_assets")
     }
   },
   root: "./client",
